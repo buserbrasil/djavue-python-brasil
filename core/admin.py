f54b5948ffa1213e1fa6fa6f0eb71ca51204266b
renzon <renzon@gmail.com>
2021-10-15 19:42:58 -0300

    Criado admin de Tweet

--- core/admin.py
+++ core/admin.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
 
-from core.models import ActivityLog, Todo
+from core.models import ActivityLog, Todo, Tweet
 
 
 class ActivityLogAdmin(admin.ModelAdmin):
     list_display = ('type', 'logged_user', 'created_at')
 
+
 class TodoAdmin(admin.ModelAdmin):
     list_display = ('description', 'done')
+
+
+@admin.register(Tweet)
+class TweetAdmin(admin.ModelAdmin):
+    list_display = ('author_name', 'author_username', 'created_at')
+    fields = ('author_name', 'author_username', 'author_avatar', 'created_at', 'text')
 
 
 admin.site.register(ActivityLog, ActivityLogAdmin)
