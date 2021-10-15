041cbd029bfe7ab30d425551ce9ae189b08c7631
renzon <renzon@gmail.com>
2021-10-15 19:13:40 -0300

    Escrita de testes para view stática

--- core/models.py
+++ core/models.py
@@ -5,7 +5,8 @@ from django.contrib.auth.models import User
 class ActivityLog(models.Model):
     type = models.CharField(max_length=64)
     logged_user = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
-    fromuser = models.ForeignKey(User, null=True, blank=True, related_name="activitylogs_withfromuser", on_delete=models.SET_NULL)
+    fromuser = models.ForeignKey(User, null=True, blank=True, related_name="activitylogs_withfromuser",
+                                 on_delete=models.SET_NULL)
     jsondata = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField('criado em', auto_now_add=True)
 
@@ -30,3 +31,11 @@ class Todo(models.Model):
             'description': self.description,
             'done': self.done,
         }
+
+
+class Tweet(models.Model):
+    author_name = models.CharField(max_length=64)
+    author_username = models.CharField(max_length=64)
+    author_avatar = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    text = models.TextField()
